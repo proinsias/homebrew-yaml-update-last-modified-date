c78f738eff368a4064efff042798079a85826eff
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2023-04-18 11:05:49 -0400

    Update URL

--- Formula/yaml-update-last-modified-date.rb
+++ Formula/yaml-update-last-modified-date.rb
@@ -1,7 +1,7 @@
 class YamlUpdateLastModifiedDate < Formula
   desc "Automatically set the last modified date in YAML front matter of edited markdown files"
   homepage "https://github.com/proinsias/yaml-update-last-modified-date"
-  url "https://github.com/proinsias/yaml-update-last-modified-date/archive/refs/tags/v1.1.0.tar.gz"
+  url "https://github.com/proinsias/yaml-update-last-modified-date/archive/v1.1.0.tar.gz"
   version "v1.1.0"
   sha256 "be66271c9a109027ac70a608e7bc79490c6984f567b48f6b1ead96072d560b57"
 
