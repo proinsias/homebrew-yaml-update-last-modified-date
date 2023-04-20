299fdea03156637d22704f8b32ac60703418fa2a
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2023-04-20 17:40:03 -0400

    Address linter warnings

--- Formula/yaml-update-last-modified-date.rb
+++ Formula/yaml-update-last-modified-date.rb
@@ -1,8 +1,8 @@
 class YamlUpdateLastModifiedDate < Formula
-  desc "Automatically set the last modified date in YAML front matter of edited markdown files"
+  desc "Set the last modified date in YAML front matter of edited markdown files"
   homepage "https://github.com/proinsias/yaml-update-last-modified-date"
   url "https://github.com/proinsias/yaml-update-last-modified-date/archive/v1.1.1.tar.gz"
-  version "v1.1.1"
+  version "1.1.1"
   sha256 "69c69c0b5ab4ca08230a6617473b9fed1af986bf7e5ba1a998b37135f6fe05af"
 
   def install
