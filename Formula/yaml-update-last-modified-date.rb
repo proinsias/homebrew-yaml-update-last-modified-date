b4de3ff809b182d4a01a9344cc470b0634a88264
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2023-04-20 17:46:19 -0400

    Update yaml-update-last-modified-date.rb

--- Formula/yaml-update-last-modified-date.rb
+++ Formula/yaml-update-last-modified-date.rb
@@ -2,7 +2,6 @@ class YamlUpdateLastModifiedDate < Formula
   desc "Set the last modified date in YAML front matter of edited markdown files"
   homepage "https://github.com/proinsias/yaml-update-last-modified-date"
   url "https://github.com/proinsias/yaml-update-last-modified-date/archive/v1.1.1.tar.gz"
-  version "1.1.1"
   sha256 "69c69c0b5ab4ca08230a6617473b9fed1af986bf7e5ba1a998b37135f6fe05af"
 
   def install
